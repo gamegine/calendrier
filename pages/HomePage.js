7bfb9413e3e3150c4a58fdb1060fa7127e7e2806
Tanguy Ralaimiadana <tanguy.rdn@gmail.com>
2019-11-07 15:15:23 +0100

    agenda mode

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -7,12 +7,23 @@ class HomePage extends React.Component {
 
     render(){
         return(
-            <View style={{ flex: 1 }}>
-                <View style={{flex: 1}}></View>
-                <View style={{flex: 3}}>
-                    <Calendar/>
-                </View>
-            </View>
+            //<View style={{ flex: 1 }}>
+                //<View style={{flex: 3}}>
+                    <Agenda 
+                    items={{
+                        '2019-11-07':[{text: 'projet dev mobile pour le 14/11'}]
+                    }}
+                    renderItem={(item, firstItemInDay) => {return (<View />);}}
+                    renderDay={(day, item) => {return (<View />);}}
+                    rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+                    style={{}}
+
+                    />
+                    
+                    
+                    
+                //</View>
+            //</View>
         )
     }
 }
