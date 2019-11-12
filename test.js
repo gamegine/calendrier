a8b1c9a0d4c288b4260b0b5a8aa4d2297715c374
gamegine <gamegine@protonmail.com>
2019-11-12 23:16:41 +0100

    google api rest crud event

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
+import calendar from './serices/google'
 
+import { google } from './config.json'
 
-const mapStateToProps = (state) => ({state})
+const mapStateToProps = (state) => ({ state })
 const mapDispatchToProps = (dispatch) => {
     return {
         dispatch: (action) => { dispatch(action) }
@@ -11,11 +13,26 @@ const mapDispatchToProps = (dispatch) => {
 }
 export default connect(mapStateToProps, mapDispatchToProps)(class WeatherComp extends React.Component {
     componentDidMount() {
-        this.props.dispatch({ type: 'INIT', value: [] })
+        //this.props.dispatch({ type: 'INIT', value: [] })
+
+        const resource = {
+            "summary": "test",
+            "location": "Somewhere",
+            "start": {
+                "dateTime": "2019-11-15T10:00:00.000-07:00"
+            },
+            "end": {
+                "dateTime": "2019-11-15T11:30:00.000-07:00"
+            }
+        };
+        const cal = new calendar(google.url, google.token);
+        //cal.events.list({'calendarId': 'primary'}).then(console.log)
+        //cal.events.insert({'calendarId': 'primary','resource': resource}).then(console.log)
+        //cal.events.update({'calendarId': 'primary','eventId':'08dmuabk2321c63cug3an6f536','resource': resource}).then(console.log)
+        //cal.events.delete({'calendarId': 'primary','eventId':'1aoimsfqijbcd8qguobnu3a50t'}).then(console.log)
     }
 
     render() {
-        console.log("\n\n=========",this.props)
         return (<Text>test</Text>);
     }
 })
