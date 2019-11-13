95779358d4454329dbfb16d8c8d3678a444ae89a
gamegine <gamegine@protonmail.com>
2019-11-13 17:41:35 +0100

    google-sync start

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { initAsync, createAsync, updateAsync, deleteAsync } from './redux/actions/event'
+import testAsync from './redux/actions/google-sync'
 
 import { AsyncStorage } from 'react-native';
 
@@ -23,29 +24,11 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(class WeatherComp extends React.Component {
-    componentDidMount() {
-        //AsyncStorage.removeItem("event").then(console.log("storage remove event"))
-        //this.props.actions.init()
-        //this.props.actions.create({ "id": 1, "test": "test" })
-        //this.props.actions.update({ "id": 1, "test": "test1" })
-        //this.props.actions.delete(1)
-        //this.props.dispatch({ type: 'INIT', value: [] })
+    state = {
+    }
 
-        const resource = {
-            "summary": "test",
-            "location": "Somewhere",
-            "start": {
-                "dateTime": "2019-11-15T10:00:00.000-07:00"
-            },
-            "end": {
-                "dateTime": "2019-11-15T11:30:00.000-07:00"
-            }
-        };
-        const cal = new calendar(google.url, google.token);
-        //cal.events.list({'calendarId': 'primary'}).then(console.log)
-        //cal.events.insert({'calendarId': 'primary','resource': resource}).then(console.log)
-        //cal.events.update({'calendarId': 'primary','eventId':'08dmuabk2321c63cug3an6f536','resource': resource}).then(console.log)
-        //cal.events.delete({'calendarId': 'primary','eventId':'1aoimsfqijbcd8qguobnu3a50t'}).then(console.log)
+    componentDidMount() {
+        testAsync([]).then(console.log)
     }
 
     render() {
