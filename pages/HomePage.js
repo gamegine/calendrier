31d25535a4eb93d15d9153ea5fae65cf325e6961
gamegine <gamegine@protonmail.com>
2019-11-13 12:29:51 +0100

    some fix

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,104 +1,67 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image, FlatList } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import {LocaleConfig} from 'react-native-calendars';
+import { Text, StyleSheet, View } from 'react-native';
+import { Agenda } from 'react-native-calendars';
 
 export default class HomePage extends React.Component {
+  state = {
+    items: {}
+  };
+  render() {
+    return (
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+      />
+    );
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          items: {}
-        };
-      }
+  loadItems(day) {
+    console.log(`Load Items for ${day.year}-${day.month} : ${day.dateString}`);
+    setTimeout(() => {
+      curent = this.state.items
+      for (let i = 1; i <= 31; i++) { curent[`${day.year}-${day.month}-${(i >= 10) ? i : "0" + i}`] = [] }
+      curent[`${day.year}-${day.month}-15`] = [{ "height": 50, "name": "test" }, { "height": 50, "name": "test" }]
+      if(this.props.items !== curent) this.setState({ items: curent })
+    }, 1000);
+  }
 
-    render(){
-        /*let test = {
-            '2019-11-07':[],
-            '2019-11-09':[]
-        }
-        test[ (new Date()).toISOString().slice(0,10) ]=[{text:"test"}]
-        console.log(test)*/
-        return(
-            //<View style={{ flex: 1 }}>
-                //<View style={{flex: 3}}>
-                    <Agenda 
-                    items={this.state.items}
-                    loadItemsForMonth={this.loadItems.bind(this)}
-                    
-                    renderItem={this.renderItem.bind(this)}
-                    renderEmptyDate={this.renderEmptyDate.bind(this)}
-                    rowHasChanged={this.rowHasChanged.bind(this)}
+  renderItem(item) {
+    return (
+      <View style={[styles.item, { height: item.height }]}><Text>{item.name}</Text></View>
+    );
+  }
 
-                    />
-                    
-                    
-                    
-                //</View>
-            //</View>
-        );
-    }
+  renderEmptyDate() {
+    return (
+      <View style={styles.item}><Text>Pas d'évènemement enregistré pour cette date.</Text></View>
+    );
+  }
 
-    loadItems(day) {
-        setTimeout(() => {
-          for (let i = -15; i < 85; i++) {
-            const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-            const strTime = this.timeToString(time);
-            if (!this.state.items[strTime]) {
-              this.state.items[strTime] = [];
-              const numItems = Math.floor(Math.random() * 5);
-              for (let j = 0; j < numItems; j++) {
-                this.state.items[strTime].push({
-                  name: 'Item for ' + strTime,
-                  height: Math.max(50, Math.floor(Math.random() * 150))
-                });
-              }
-            }
-          }
-          //console.log(this.state.items);
-          const newItems = {};
-          Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-          this.setState({
-            items: newItems
-          });
-        }, 1000);
-        // console.log(`Load Items for ${day.year}-${day.month}`);
-      }
-    
-      renderItem(item) {
-        return (
-          <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
-        );
-      }
-    
-      renderEmptyDate() {
-        return (
-          <View style={styles.item}><Text>Pas d'évènemement enregistré pour cette date.</Text></View>
-        );
-      }
-    
-      rowHasChanged(r1, r2) {
-        return r1.name !== r2.name;
-      }
-    
-      timeToString(time) {
-        const date = new Date(time);
-        return date.toISOString().split('T')[0];
-      }
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
 }
 
 const styles = StyleSheet.create({
-    item: {
-      backgroundColor: 'white',
-      flex: 1,
-      borderRadius: 5,
-      padding: 10,
-      marginRight: 10,
-      marginTop: 17
-    },
-    emptyDate: {
-      height: 15,
-      flex:1,
-      paddingTop: 30
-    }
-  });
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30
+  }
+});
