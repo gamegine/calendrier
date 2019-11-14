1d0a348623a83641491bedc2d288266b9e719e28
gamegine <gamegine@protonmail.com>
2019-11-14 03:49:23 +0100

    form start

--- pages/eventForm.js
+++ pages/eventForm.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, StatusBar, TextInput, View, StyleSheet, DatePickerIOS } from 'react-native';
 export default class EventForm extends React.Component {
+  state = { name: '' }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Paramètres</Text>
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <TextInput
+          style={styles.input}
+          value={this.state.name}
+          onChangeText={name => this.setState({ name })}
+          placeholder="Name"
+          autoFocus={true}
+          autoCorrect={true}
+          keyboardType="default"
+        />
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 20,
+    marginBottom: 0,
+    height: 34,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    fontSize: 16,
+  }
+});
